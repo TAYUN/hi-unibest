50e33b0266f9c53e4a70a85f46924e402399cee6
weiye <507028106@qq.com>
2025-07-19 14:29:31 +0800

    feat: init project

---
+++ src/pages/about/about.vue
@@ -0,0 +1,62 @@
+<route lang="jsonc" type="page">
+{
+  "layout": "tabbar",
+  "style": {
+    "navigationBarTitleText": "关于"
+  }
+}
+</route>
+
+<script lang="ts" setup>
+import RequestComp from './components/request.vue'
+
+// 奇怪：同样的代码放在 vue 里面不会校验到错误，放在 .ts 文件里面会校验到错误
+// const testOxlint = (name: string) => {
+//   console.log('oxlint')
+// }
+// testOxlint('oxlint')
+console.log('about')
+
+function gotoAlova() {
+  uni.navigateTo({
+    url: '/pages/about/alova',
+  })
+}
+function gotoVueQuery() {
+  uni.navigateTo({
+    url: '/pages/about/vue-query',
+  })
+}
+</script>
+
+<template>
+  <view>
+    <view class="mt-8 text-center text-3xl">
+      鸽友们好，我是
+      <text class="text-red-500">
+        菲鸽
+      </text>
+    </view>
+    <RequestComp />
+    <view class="text-center">
+      <button type="primary" size="mini" class="w-160px" @click="gotoAlova">
+        前往 alova 示例页面
+      </button>
+    </view>
+    <view class="text-center">
+      <button type="primary" size="mini" class="w-160px" @click="gotoVueQuery">
+        vue-query 示例页面
+      </button>
+    </view>
+  </view>
+</template>
+
+<style lang="scss" scoped>
+.test-css {
+  // 16rpx=>0.5rem
+  padding-bottom: 16rpx;
+  // mt-4=>1rem=>16px;
+  margin-top: 16px;
+  text-align: center;
+}
+</style>
